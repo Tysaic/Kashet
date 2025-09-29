772d8810303bb3a4e5d7ce085a7310b7e7bcd855
Tysaic <mendezlinaresi@gmail.com>
2025-09-29 15:21:15 -0300

    Added templates with bussines workflow

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,16 @@ urlpatterns = [
     path('', views.root_redirect, name='root_redirect'),  # redirect from /
     path('index', views.index, name='index'),
     path('budget', views.budget, name='budget'),
+    path('budget-details', views.details_budget, name='details_budget'),
+    path('budget-add', views.add_budget, name='add_budget'),
+    path('bills', views.bills, name="bills"),
+    path('bills-categories', views.categories_bills, name="categories_bills"),
+    path('bills-reports', views.bills_reports, name="bills_reports"),
+    path('bills-add', views.bills_add, name="bills_add"),
+    path('departments', views.departments, name="departments"),
+    path('departments-add', views.departments_add, name="departments_add"),
+    path('roles', views.roles, name="roles"),
+    path('roles-add', views.roles_add, name="roles_add"),
+    path('roles-users', views.roles_users, name="roles_users"),
+    path('roles-users-add', views.roles_users_add, name="roles_users_add"),
 ]
--- app/views.py
+++ app/views.py
@@ -9,4 +9,40 @@ def index (request):
     return render(request, 'index.html')
 
 def budget(request):
-    return render(request, 'app/budget.html')
+    return render(request, 'app/budgets/budget.html')
+
+def details_budget(request):
+    return render(request, 'app/budgets/details_budget.html')
+
+def add_budget(request):
+    return render(request, 'app/budgets/add_budget.html')
+
+def bills(request):
+    return render(request, 'app/bills/bills.html')
+
+def categories_bills(request):
+    return render(request, 'app/bills/bills_categories.html')
+
+def bills_reports(request):
+    return render(request, 'app/bills/bills_reports.html')
+
+def bills_add(request):
+    return render(request, 'app/bills/bills_add.html')
+
+def departments(request):
+    return render(request, 'app/departments/departments.html')
+
+def departments_add(request):
+    return render(request, 'app/departments/departments_add.html')
+
+def roles(request):
+    return render(request, 'app/roles/roles.html')
+
+def roles_add(request):
+    return render(request, 'app/roles/roles_add.html')
+
+def roles_users(request):
+    return render(request, 'app/roles/roles_users.html')
+
+def roles_users_add(request):
+    return render(request, 'app/roles/roles_users_add.html')
