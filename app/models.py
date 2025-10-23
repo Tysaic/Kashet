e4eac801ffe7360b649421dde19624b85ce5471d
Tysaic <mendezlinaresi@gmail.com>
2025-10-23 18:58:52 -0300

    Uploading budget model and uploader file with testing

--- app/models.py
+++ app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import uuid
+import os
 
-# Create your models here.
+def budget_upload_path(instance, filename):
+    return os.path.join('budgets', str(instance.budget.identifier), filename)
+
+class Budget(models.Model):
+    CURRENCIES = [
+        ('CLP', 'Chilean Peso ($)'),
+        ('USD', 'US Dollar ($)'),
+        ('EUR', 'Euro (€)')
+    ]
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    total_mount = models.DecimalField(max_digits=24, decimal_places=2)
+    identifier = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    currency = models.CharField(max_length=3, choices=CURRENCIES, default=CURRENCIES[0][0])
+    created_at = models.DateField(auto_now_add=True)
+    due_date = models.DateField(null=True, blank=True)
+    enable = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.name} - {self.description} - ({self.total_mount} {self.currency})"
+    
+
+class BudgetFile(models.Model):
+    budget = models.ForeignKey(Budget, related_name="upload_folders", on_delete=models.CASCADE)
+    file = models.FileField(upload_to=budget_upload_path)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
+
+
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,66 @@
 from django.test import TestCase
+from datetime import date
+from .models import Budget, BudgetFile
+from django.core.files.uploadedfile import SimpleUploadedFile
+import uuid
 
-# Create your tests here.
+
+
+class BudgetModelTest(TestCase):
+
+    def setUp(self):
+        self.budget = Budget.objects.create(
+            name = "Marketing Budget 01 Testing",
+            description = "Social media and campaings",
+            total_mount = 1500.0,
+            currency = "CLP",
+            identifier = uuid.uuid4(),
+            due_date = date(2026,11,30)
+        )
+    
+    def test_budget_creation(self):
+        """Ensure the budget object is created correctly."""
+        today = date.today()
+        self.assertEqual(self.budget.name, "Marketing Budget 01 Testing")
+        self.assertEqual(self.budget.description, "Social media and campaings")
+        self.assertEqual(self.budget.total_mount, 1500.0)
+        self.assertEqual(self.budget.currency, "CLP")
+        self.assertTrue(self.budget.enable)
+        self.assertEqual(self.budget.created_at, today)
+        self.assertEqual(self.budget.due_date, date(2026,11,30))
+        self.assertIsInstance(self.budget.identifier, uuid.UUID)
+    
+    def test_budget_model_str(self):
+        expected = "Marketing Budget 01 Testing - Social media and campaings - (1500.0 CLP)"
+        self.assertEqual(str(self.budget), expected)
+        
+class BudgetFileModelTest(TestCase):
+
+    def setUp(self):
+        self.budget = Budget.objects.create(
+            name = "File Uploader 1",
+            description = "File uploader description 1",
+            total_mount = 2000.0,
+            currency = "CLP",
+            identifier = uuid.uuid4(),
+            due_date = date(2026,11,30)
+        )
+    
+        self.file = SimpleUploadedFile(
+            "test_document.pdf",
+            b"dummy file content",
+            content_type="application/pdf"
+        )
+
+        self.budget_file = BudgetFile.objects.create(
+            budget = self.budget,
+            file=self.file
+        )
+
+    def test_file_upload_relation(self):
+        self.assertEqual(self.budget.upload_folders.count(), 1)
+        self.assertEqual(self.budget.upload_folders.first().file.name.split('/')[-1], "test_document.pdf")
+    
+    def test_budgetfile_str(self):
+        filename = self.budget_file.file.name.split('/')[-1]
+        self.assertEqual(str(self.budget_file), f"budgets/{self.budget.identifier}/{filename}")
